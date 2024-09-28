9577218794008583323f9f83fc9edca69c1d8d93
Md.Abdur Rahman <ar.minhazul@gmail.com>
2024-09-28 21:24:39 +0600

    Create automated tests for orderSummary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -30,7 +30,7 @@ export function renderOrderSummary(){
         
 
         cartSummaryHTML+= `
-        <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+        <div class="cart-item-container js-cart-item-container js-cart-item-container-${matchingProduct.id}">
                 <div class="delivery-date">
                 Delivery date: ${dateString}
                 </div>
@@ -46,14 +46,14 @@ export function renderOrderSummary(){
                     <div class="product-price">
                     $${formatCurrency(matchingProduct.priceCents)}
                     </div>
-                    <div class="product-quantity">
+                    <div class="product-quantity js-product-quantity-${matchingProduct.id}">
                     <span>
                         Quantity: <span class="quantity-label">${cartItem.quantity}</span>
                     </span>
                     <span class="update-quantity-link link-primary">
                         Update
                     </span>
-                    <span class="delete-quantity-link link-primary js-delete-link " data-product-id="${matchingProduct.id}">
+                    <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id} " data-product-id="${matchingProduct.id}">
                         Delete
                     </span>
                     </div>
@@ -63,6 +63,7 @@ export function renderOrderSummary(){
                     <div class="delivery-options-title">
                     Choose a delivery option:
                     </div>
+                    <div class="js-payment-summary"></div>
                     ${deliveryOptionsHTML(matchingProduct,cartItem)}
                 
                 </div>
@@ -113,7 +114,7 @@ export function renderOrderSummary(){
 
             const container=  document.querySelector(`.js-cart-item-container-${productId}`);
             container.remove();
-            renderPaymentSummary()
+            renderPaymentSummary();
 
         })
     })
